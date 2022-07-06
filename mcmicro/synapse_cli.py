930e94989f235515bf1aeb20486c8ac6091e3228
Wouter-Michiel <wv181992@mygithub.gsk.com>
2022-07-06 09:42:50 -0400

    Solely download CODEX

--- mcmicro/synapse_cli.py
+++ mcmicro/synapse_cli.py
@@ -4,13 +4,15 @@ import argparse
 from pathlib import Path
 
 def main(username, password):
-    mcmicro_dir = Path().resolve() / "data"
-    mcmicro_dir.mkdir(parents=True, exist_ok=True)
+    wsi_codex_dir = Path().resolve() / "data" / "WSI_tonsil" / "CODEX"
+    wsi_codex_dir.mkdir(parents=True, exist_ok=True)
 
     syn = synapseclient.Synapse() 
     syn.login(username, password) 
-    files = synapseutils.syncFromSynapse(syn, 'syn24849819', path=mcmicro_dir)
-    
+    files = synapseutils.syncFromSynapse(syn, 'syn25173958', path=wsi_codex_dir)
+    # files = synapseutils.syncFromSynapse(syn, 'syn22345748', path=tma_dir)
+
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='CLI for downloading the MCMICRO tonsil dataset')
     parser.add_argument('-u',
