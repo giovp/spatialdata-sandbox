f9e73865089860d624524c76d0069fb7f3cd03d3
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2022-12-09 10:34:03 +0100

    added tests for napari, fixed path for vscode

---
+++ tests/test_napari.py
@@ -0,0 +1,38 @@
+import os
+from spatialdata import SpatialData
+from napari_spatialdata import Interactive
+
+
+def test_merfish():
+    sdata = SpatialData.read("merfish/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_mibitof():
+    sdata = SpatialData.read("mibitof/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_nanostring_cosmx():
+    sdata = SpatialData.read("nanostring_cosmx/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_toy():
+    sdata = SpatialData.read("toy/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_visium():
+    sdata = SpatialData.read("visium/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_visium2():
+    sdata = SpatialData.read("visium2/data.zarr")
+    Interactive(sdata, headless=True)
+
+
+def test_xenium():
+    sdata = SpatialData.read("xenium/data.zarr")
+    Interactive(sdata, headless=True)
--- tests/test_to_zarr.py
+++ tests/test_to_zarr.py
@@ -1,38 +1,40 @@
 import os
+import pathlib
 
+path = pathlib.Path(__file__).parent.parent.resolve()
 
 def test_merfish():
-    os.chdir("../merfish")
+    os.chdir(path / "merfish")
     import merfish.to_zarr
 
 
 def test_mibitof():
-    os.chdir("../mibitof")
+    os.chdir(path / "mibitof")
     import mibitof.to_zarr
 
 
 def test_nanostring_cosmx():
-    os.chdir("../nanostring_cosmx")
+    os.chdir(path / "nanostring_cosmx")
     import nanostring_cosmx.to_zarr
 
 
 def test_toy():
-    os.chdir("../toy")
+    os.chdir(path / "toy")
     import toy.to_zarr
 
 
 def test_visium():
-    os.chdir("../visium")
+    os.chdir(path / "visium")
     import visium.to_zarr
 
 
 def test_visium2():
-    os.chdir("../visium2")
+    os.chdir(path / "visium2")
     import visium2.to_zarr
 
 
 def test_xenium():
-    os.chdir("../xenium")
+    os.chdir(path / "xenium")
     from xenium.to_zarr import main
 
     main()
