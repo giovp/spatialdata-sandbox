46f771569263d845ffff69f82f20dce0955654ed
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2023-04-04 03:35:41 +0200

    wip deep learning example, accuracy 0.69

--- examples/xenium_and_visium.py
+++ examples/xenium_and_visium.py
@@ -44,101 +44,93 @@ from pytorch_lightning import LightningDataModule
 ##
 SMALL_DATASET = False
 
-# loading the Xenium and Visium SpatialData objects
-print("current working directory:", os.getcwd())
-SPATIALDATA_SANDBOX_PATH = "."
-GENERATED_DATA_PATH = os.path.join(
-    SPATIALDATA_SANDBOX_PATH, "generated_data/xenium_visium_integration"
-)
-assert os.path.isdir(
-    GENERATED_DATA_PATH
-), f"{GENERATED_DATA_PATH} not found, please use symlinks to make it available"
 
-XENIUM_SDATA_PATH = os.path.join(SPATIALDATA_SANDBOX_PATH, "xenium_rep1_io/data.zarr")
-VISIUM_SDATA_PATH = os.path.join(
-    SPATIALDATA_SANDBOX_PATH, "visium_associated_xenium_io/data.zarr"
-)
+def load_data() -> tuple[SpatialData, SpatialData, SpatialData]:
+    # loading the Xenium and Visium SpatialData objects
+    print("current working directory:", os.getcwd())
+    SPATIALDATA_SANDBOX_PATH = "."
+    GENERATED_DATA_PATH = os.path.join(
+        SPATIALDATA_SANDBOX_PATH, "generated_data/xenium_visium_integration"
+    )
+    assert os.path.isdir(
+        GENERATED_DATA_PATH
+    ), f"{GENERATED_DATA_PATH} not found, please use symlinks to make it available"
 
-assert os.path.isdir(XENIUM_SDATA_PATH)
-assert os.path.isdir(VISIUM_SDATA_PATH)
-
-xenium_sdata = sd.read_zarr(XENIUM_SDATA_PATH)
-visium_sdata = sd.read_zarr(VISIUM_SDATA_PATH)
-
-
-VISIUM_CLONAL_DATA = os.path.join(GENERATED_DATA_PATH, "visium_copyKat.h5ad")
-assert os.path.isfile(VISIUM_CLONAL_DATA)
-adata = sc.read_h5ad(VISIUM_CLONAL_DATA)
-##
-# 1. ensuring the data is aligned (i.e. that the coordinate system "aligned" is present both for the Xenium and Visium
-#    data
-# 2. removing unnecessary coordinate systems
-for sdata in [xenium_sdata, visium_sdata]:
-    aligned = False
-    all_elements = list(sdata._gen_elements())
-    for element_type, name, element in all_elements:
-        all_transformations = get_transformation(element, get_all=True)
-        if "aligned" in all_transformations:
-            set_transformation(
-                element, {"aligned": all_transformations["aligned"]}, set_all=True
-            )
-            aligned = True
-        else:
-            print(
-                "element",
-                element_type,
-                name,
-                'is not needed because it has no transformation to the "aligned" coordinate system, removing it',
-            )
-            del sdata.__getattribute__(element_type)[name]
-    if not aligned:
-        raise ValueError(
-            "No transformation to the 'aligned' coordinate system was found in any element of the spatial data. "
-            "Please run the appropriate notebook to aligned the data before running the current code."
-        )
-
-##
-# create a dataset with tiles, one for each Visium circle, but around the Xenium image
-merged = sd.SpatialData(
-    images={
-        "xenium": xenium_sdata.images["morphology_mip"],
-        "visium": visium_sdata.images["CytAssist_FFPE_Human_Breast_Cancer_full_image"],
-    },
-    shapes={
-        "CytAssist_FFPE_Human_Breast_Cancer": visium_sdata.shapes[
-            "CytAssist_FFPE_Human_Breast_Cancer"
-        ]
-    },
-    table=visium_sdata.table,
-)
-
-s = adata.obs["clone"]
-s.index = sdata.table.obs.index
-visium_sdata.table.obs["clone"] = s
-
-if SMALL_DATASET:
-    # subsetting the data for debug/development purposes
-    min_coordinate = [12790, 12194]
-    max_coordinate = [15100, 14221]
-    merged = merged.query.bounding_box(
-        min_coordinate=min_coordinate,
-        max_coordinate=max_coordinate,
-        axes=["y", "x"],
-        target_coordinate_system="aligned",
+    XENIUM_SDATA_PATH = os.path.join(
+        SPATIALDATA_SANDBOX_PATH, "xenium_rep1_io/data.zarr"
+    )
+    VISIUM_SDATA_PATH = os.path.join(
+        SPATIALDATA_SANDBOX_PATH, "visium_associated_xenium_io/data.zarr"
     )
 
-circles = merged["CytAssist_FFPE_Human_Breast_Cancer"]
-t = get_transformation(circles, "aligned")
+    assert os.path.isdir(XENIUM_SDATA_PATH)
+    assert os.path.isdir(VISIUM_SDATA_PATH)
 
-transformed_circles = transform(circles, t)
-visium_circle_diameter = 2 * transformed_circles.radius.iloc[0]
-dataset = ImageTilesDataset(
-    sdata=merged,
-    regions_to_images={"CytAssist_FFPE_Human_Breast_Cancer": "xenium"},
-    tile_dim_in_units=visium_circle_diameter,
-    tile_dim_in_pixels=32,
-    target_coordinate_system="aligned",
-)
+    xenium_sdata = sd.read_zarr(XENIUM_SDATA_PATH)
+    visium_sdata = sd.read_zarr(VISIUM_SDATA_PATH)
+
+    VISIUM_CLONAL_DATA = os.path.join(GENERATED_DATA_PATH, "visium_copyKat.h5ad")
+    assert os.path.isfile(VISIUM_CLONAL_DATA)
+    adata = sc.read_h5ad(VISIUM_CLONAL_DATA)
+    ##
+    # 1. ensuring the data is aligned (i.e. that the coordinate system "aligned" is present both for the Xenium and Visium
+    #    data
+    # 2. removing unnecessary coordinate systems
+    for sdata in [xenium_sdata, visium_sdata]:
+        aligned = False
+        all_elements = list(sdata._gen_elements())
+        for element_type, name, element in all_elements:
+            all_transformations = get_transformation(element, get_all=True)
+            if "aligned" in all_transformations:
+                set_transformation(
+                    element, {"aligned": all_transformations["aligned"]}, set_all=True
+                )
+                aligned = True
+            else:
+                print(
+                    "element",
+                    element_type,
+                    name,
+                    'is not needed because it has no transformation to the "aligned" coordinate system, removing it',
+                )
+                del sdata.__getattribute__(element_type)[name]
+        if not aligned:
+            raise ValueError(
+                "No transformation to the 'aligned' coordinate system was found in any element of the spatial data. "
+                "Please run the appropriate notebook to aligned the data before running the current code."
+            )
+    ##
+    # create a dataset with tiles, one for each Visium circle, but around the Xenium image
+    merged = sd.SpatialData(
+        images={
+            "xenium": xenium_sdata.images["morphology_mip"],
+            "visium": visium_sdata.images[
+                "CytAssist_FFPE_Human_Breast_Cancer_full_image"
+            ],
+        },
+        shapes={
+            "CytAssist_FFPE_Human_Breast_Cancer": visium_sdata.shapes[
+                "CytAssist_FFPE_Human_Breast_Cancer"
+            ]
+        },
+        table=visium_sdata.table,
+    )
+
+    s = adata.obs["clone"]
+    s.index = sdata.table.obs.index
+    visium_sdata.table.obs["clone"] = s
+
+    if SMALL_DATASET:
+        # subsetting the data for debug/development purposes
+        min_coordinate = [12790, 12194]
+        max_coordinate = [15100, 14221]
+        merged = merged.query.bounding_box(
+            min_coordinate=min_coordinate,
+            max_coordinate=max_coordinate,
+            axes=["y", "x"],
+            target_coordinate_system="aligned",
+        )
+    return merged, visium_sdata, xenium_sdata
 
 
 def _get_clonal_classes(adata: AnnData) -> list[str]:
@@ -163,58 +155,55 @@ class ClonalTileDataset(torch.utils.data.Dataset):
         clonal_one_hot = F.one_hot(
             torch.tensor(clonal_information), num_classes=len(self.categories)
         )
-        return tile, clonal_one_hot.float()
+        tile_numpy = tile.data.compute().astype(np.float32)
+        return tile_numpy, clonal_one_hot.float()
 
 
-clonal_tile_dataset = ClonalTileDataset(dataset)
+def get_dataset() -> ImageTilesDataset:
+    merged, xenium_sdata, visium_sdata = load_data()
 
-##
-# visualization (only if using the small dataset, otherwise the code will be too slow)
-if SMALL_DATASET:
-    tiles = []
-    for i, tile in enumerate(tqdm(dataset)):
-        tiles.append(tile)
+    circles = merged["CytAssist_FFPE_Human_Breast_Cancer"]
+    t = get_transformation(circles, "aligned")
 
-    tiles_sdata = SpatialData(
-        images={
-            "full": visium_sdata.images["CytAssist_FFPE_Human_Breast_Cancer_full_image"]
-        }
-        | {
-            f"tile_{region_name}_{region_index}": tile
-            for (tile, region_name, region_index) in tiles
-        }
+    transformed_circles = transform(circles, t)
+    visium_circle_diameter = 2 * transformed_circles.radius.iloc[0]
+    dataset = ImageTilesDataset(
+        sdata=merged,
+        regions_to_images={"CytAssist_FFPE_Human_Breast_Cancer": "xenium"},
+        tile_dim_in_units=visium_circle_diameter,
+        tile_dim_in_pixels=128,
+        target_coordinate_system="aligned",
     )
+    ##
+    # visualization (only if using the small dataset, otherwise the code will be too slow)
+    if SMALL_DATASET:
+        tiles = []
+        for i, tile in enumerate(tqdm(dataset)):
+            tiles.append(tile)
 
-    Interactive([tiles_sdata, merged])
-##
+        tiles_sdata = SpatialData(
+            images={
+                "full": visium_sdata.images[
+                    "CytAssist_FFPE_Human_Breast_Cancer_full_image"
+                ]
+            }
+            | {
+                f"tile_{region_name}_{region_index}": tile
+                for (tile, region_name, region_index) in tiles
+            }
+        )
 
-# set up a classifier on the tiles
-pl.seed_everything(7)
+        Interactive([tiles_sdata, merged])
+    ##
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
-BATCH_SIZE = 256 if torch.cuda.is_available() else 64
-# NUM_WORKERS = int(os.cpu_count() / 2)
-NUM_WORKERS = 1
+    clonal_tile_dataset = ClonalTileDataset(dataset)
+    return clonal_tile_dataset
 
 
 ##
 def spatial_image_to_tensor(image: SpatialImage):
-    return torch.from_numpy(image.data.compute().astype(np.float16))
-
-
-train_transform = torchvision.transforms.Compose(
-    [
-        # torchvision.transforms.RandomCrop(32, padding=4),
-        spatial_image_to_tensor,
-        torchvision.transforms.RandomHorizontalFlip(),
-    ]
-)
-
-test_transform = torchvision.transforms.Compose(
-    [
-        spatial_image_to_tensor,
-    ]
-)
+    # return torch.from_numpy(image.data.compute().astype(np.float32))
+    return image
 
 
 class TilesDataModule(LightningDataModule):
@@ -224,19 +213,18 @@ class TilesDataModule(LightningDataModule):
         num_workers: int,
         train_transform: torchvision.transforms.Compose,
         test_transform: torchvision.transforms.Compose,
-        dataset: torch.utils.data.Dataset,
     ):
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.train_transform = train_transform
         self.test_transform = test_transform
-        self.dataset = dataset
         self.train = None
         self.val = None
         self.test = None
 
     def setup(self, stage=None):
+        self.dataset = get_dataset()
         n_train = int(len(self.dataset) * 0.7)
         n_val = int(len(self.dataset) * 0.2)
         n_test = len(self.dataset) - n_train - n_val
@@ -272,20 +260,6 @@ class TilesDataModule(LightningDataModule):
             num_workers=self.num_workers,
             shuffle=False,
         )
-
-
-tiles_data_module = TilesDataModule(
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKERS,
-    train_transform=train_transform,
-    test_transform=test_transform,
-    dataset=clonal_tile_dataset,
-)
-
-tiles_data_module.setup()
-train_dl = tiles_data_module.train_dataloader()
-val_dl = tiles_data_module.val_dataloader()
-test_dl = tiles_data_module.test_dataloader()
 
 
 class DenseNetModel(pl.LightningModule):
@@ -326,22 +300,68 @@ class DenseNetModel(pl.LightningModule):
         return {"loss": loss}
 
     def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> float:
-        return self._compute_loss_from_batch(batch=batch, batch_idx=batch_idx)
+        loss = self._compute_loss_from_batch(batch=batch, batch_idx=batch_idx)
+
+        imgs, labels = batch
+        acc = self.compute_accuracy(imgs, labels)
+        # By default logs it per epoch (weighted average over batches), and returns it afterwards
+        self.log("test_acc", acc)
+
+        return loss
 
     def test_step(self, batch, batch_idx):
         imgs, labels = batch
-        preds = self.model(imgs).argmax(dim=-1)
-        acc = (labels == preds).float().mean()
+        acc = self.compute_accuracy(imgs, labels)
         # By default logs it per epoch (weighted average over batches), and returns it afterwards
         self.log("test_acc", acc)
+
+    def compute_accuracy(self, imgs, labels):
+        preds = self.model(imgs).argmax(dim=-1)
+        labels_value = torch.argmax(labels, dim=-1)
+        acc = (labels_value == preds).float().mean()
+        return acc
 
     def configure_optimizers(self) -> Adam:
         return Adam(self.model.parameters(), lr=self.hparams.learning_rate)
 
 
 if __name__ == "__main__":
+    # set up a classifier on the tiles
+    pl.seed_everything(7)
+
+    PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+    BATCH_SIZE = 512 if torch.cuda.is_available() else 64
+    # NUM_WORKERS = int(os.cpu_count() / 2)
+    NUM_WORKERS = 64
+
+    train_transform = torchvision.transforms.Compose(
+        [
+            # torchvision.transforms.RandomCrop(32, padding=4),
+            spatial_image_to_tensor,
+            torchvision.transforms.RandomHorizontalFlip(),
+        ]
+    )
+
+    test_transform = torchvision.transforms.Compose(
+        [
+            spatial_image_to_tensor,
+        ]
+    )
+    tiles_data_module = TilesDataModule(
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+        train_transform=train_transform,
+        test_transform=test_transform,
+    )
+
+    tiles_data_module.setup()
+    train_dl = tiles_data_module.train_dataloader()
+    val_dl = tiles_data_module.val_dataloader()
+    test_dl = tiles_data_module.test_dataloader()
+
     t = train_dl.__iter__().__next__()
     print(t)
+    adata = tiles_data_module.dataset.tile_dataset.sdata.table
     model = DenseNetModel(
         learning_rate=0.05, in_channels=1, num_classes=len(_get_clonal_classes(adata))
     )
@@ -355,6 +375,7 @@ if __name__ == "__main__":
             LearningRateMonitor(logging_interval="step"),
             TQDMProgressBar(refresh_rate=10),
         ],
+        log_every_n_steps=7,
     )
 
     trainer.fit(model, datamodule=tiles_data_module)
