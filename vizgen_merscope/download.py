d91f3c431d68636459730969dde189e451eb62fc
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2023-04-21 20:49:29 +0200

    fix paths

--- vizgen_merscope/download.py
+++ vizgen_merscope/download.py
@@ -5,8 +5,8 @@ from pathlib import Path
 def main():
     URL = Path("https://zenodo.org/record/7852005/files/vizgen_sample.zip")
 
-    zip_path = Path(__file__).parents[1] / "data" / URL.name
-    folder_path = Path(__file__).parents[1] / "data" / URL.stem
+    zip_path = Path(__file__).parent / "data" / URL.name
+    folder_path = Path(__file__).parent / "data" / URL.stem
     zip_path.parent.mkdir(exist_ok=True)
 
     command = f"curl {URL} --output '{zip_path}'"
--- vizgen_merscope/to_zarr.py
+++ vizgen_merscope/to_zarr.py
@@ -1,19 +1,19 @@
+import shutil
 from pathlib import Path
 
 from spatialdata_io import merfish
 
 
 def main():
-    sample_path = Path(__file__).parents[1] / "data" / "vizgen_sample"
+    sample_path = Path(__file__).parent / "data" / "vizgen_sample"
     zarr_path = Path(__file__).parent / "data.zarr"
 
-    if zarr_path.exists():
-        print(f"Zarr folder already existing at {zarr_path}. Exiting.")
-        return
-
     sdata = merfish(sample_path, vpt_outputs=sample_path / "vpt-run")
+
+    if zarr_path.exists():
+        shutil.rmtree(zarr_path)
     sdata.write(zarr_path)
-    
+
     print(f"Saved merfish data:\n{sdata}")
 
 
