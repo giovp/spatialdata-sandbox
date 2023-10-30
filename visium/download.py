4b45da8d1fec154be27caf939860677e772814c6
Luca Marconato <m.lucalmer@gmail.com>
2023-10-30 19:19:23 +0100

    using spatialdata-io for the visium dataset

--- visium/download.py
+++ visium/download.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
 from pathlib import Path
-import scanpy as sc
 
 import sys
 
@@ -15,12 +13,6 @@ main_dir = Path().resolve() / "data"
 print(main_dir)
 main_dir.mkdir(parents=True, exist_ok=True)
 
-image_dir = main_dir / "images"
-image_dir.mkdir(parents=True, exist_ok=True)
-
-table_dir = main_dir / "tables"
-table_dir.mkdir(parents=True, exist_ok=True)
-
 
 brainfile = main_dir / "mousebrain.zip"
 download(
@@ -29,19 +21,3 @@ download(
     desc="data",
 )
 unzip(brainfile, main_dir)
-
-for lib in os.scandir(
-    main_dir / os.path.join("mouse_brain_visium_wo_cloupe_data", "rawdata")
-):
-    if not lib.name.startswith("."):
-        shutil.copy2(
-            main_dir / os.path.join(lib.path, "spatial", "tissue_hires_image.png"),
-            main_dir / os.path.join("images", lib.name + ".png"),
-        )
-
-        adata = sc.read_visium(main_dir / lib.path)
-        adata.write_h5ad(
-            main_dir / os.path.join("tables", lib.name + ".h5ad"),
-            compression="gzip",
-            compression_opts=9,
-        )
