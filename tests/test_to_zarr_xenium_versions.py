f80c236e3ece5100fc763e42457c5f8697a40ef1
Luca Marconato <m.lucalmer@gmail.com>
2024-02-22 02:00:48 +0100

    added tests for all the xenium versions

---
+++ tests/test_to_zarr_xenium_versions.py
@@ -0,0 +1,40 @@
+import pathlib
+
+
+path = pathlib.Path(__file__).parent.parent.resolve()
+
+import subprocess
+import pytest
+
+
+# TODO: this code is untested, test that it works
+class TestToZarrScripts:
+    def execute_script(self, script):
+        result = subprocess.run(
+            ["python", script],
+            capture_output=True,
+            cwd=script.parent,
+            text=True,
+        )
+
+        # Check that script finished successfully (returncode 0)
+        if result.returncode != 0:
+            print(
+                f"Script {script} failed with output:\n{result.stdout}\n{result.stderr}"
+            )
+        assert result.returncode == 0
+
+    @pytest.mark.parametrize(
+        "script",
+        [
+            "xenium_1.0.2_io/to_zarr.py",
+            "xenium_1.3.0_io/to_zarr.py",
+            "xenium_1.4.0_io/to_zarr.py",
+            "xenium_1.5.0_io/to_zarr.py",
+            "xenium_1.6.0_io/to_zarr.py",
+            "xenium_1.7.0_io/to_zarr.py",
+            "xenium_2.0.0_io/to_zarr.py",
+        ],
+    )
+    def test_xenium_to_zarr_scripts(self, script):
+        self.execute_script(path / script)
