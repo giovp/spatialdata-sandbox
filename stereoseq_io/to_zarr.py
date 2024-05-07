104054130a0ec0a8f549c8e9294d4eb197b182dd
Luca Marconato <m.lucalmer@gmail.com>
2024-05-07 16:46:30 +0200

    fixed path stereoseq

--- stereoseq_io/to_zarr.py
+++ stereoseq_io/to_zarr.py
@@ -10,7 +10,9 @@ if not str(path).endswith("stereoseq_io"):
     path /= "stereoseq_io"
     assert path.exists()
 
-dataset = Path("C://STT1_image_alignment//pipeline output//result")
+# you can use symlinks to make the data available
+# C://STT1_image_alignment//pipeline output//result"
+dataset = Path("data/STT1_image_alignment/pipeline output/result")
 
 path_read = path / dataset
 sdata = stereoseq(path_read)
