776ec9010ac230797be6baba236e62ddcac3a630
Luca Marconato <m.lucalmer@gmail.com>
2024-06-12 16:13:25 +0200

    visium hd on-the-fly rasterization example

--- examples/rasterize_bins.py
+++ examples/rasterize_bins.py
@@ -1,4 +1,5 @@
 ##
+import time
 from spatialdata import read_zarr, rasterize_bins
 
 f = '../visium_hd_3.0.0_io/data.zarr'
@@ -6,15 +7,26 @@ f = '../visium_hd_3.0.0_io/data.zarr'
 sdata = read_zarr(f)
 
 ##
-sdata['square_002um'].X = sdata['square_002um'].X.tocsc()
-while True:
-    var_names = ["Epcam", "Cd8a", "Cd4"]
-    rasterize_bins(
-        sdata,
-        "Visium_HD_Mouse_Small_Intestine_square_002um",
-        "square_002um",
-        "array_col",
-        "array_row",
-        value_key=var_names,
-    )
-    print('done')
+# bins = '002'
+bins = '016'
+sdata[f'square_{bins}um'].X = sdata[f'square_{bins}um'].X.tocsc()
+var_names = ["Epcam", "Cd8a", "Cd4"]
+start = time.time()
+rasterized = rasterize_bins(
+    sdata,
+    f"Visium_HD_Mouse_Small_Intestine_square_{bins}um",
+    f"square_{bins}um",
+    "array_col",
+    "array_row",
+    # value_key=var_names,
+)
+print(f"rasterize: {time.time() - start}")
+print('done')
+
+from napari_spatialdata import Interactive
+sdata['rasterized'] = rasterized
+interactive = Interactive(sdata, headless=True)
+interactive.add_element(element='rasterized', element_coordinate_system='global', view_element_system=True)
+interactive.run()
+
+##
