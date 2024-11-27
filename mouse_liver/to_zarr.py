a7061eb155a85e0ab470c132375cbf0c0c0f571a
Luca Marconato <m.lucalmer@gmail.com>
2024-11-27 14:38:42 +0100

    fix to_zarr mouse_liver

--- mouse_liver/to_zarr.py
+++ mouse_liver/to_zarr.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 from pathlib import Path
 import shutil
+import spatialdata as sd
 
 data_dir = Path("data")
 zip_path = data_dir / "mouseLiver.zarr.zip"
@@ -9,6 +10,20 @@ zip_path = data_dir / "mouseLiver.zarr.zip"
 # Unzip the dataset into the extraction directory
 subprocess.run(["unzip", "-o", zip_path, "-d", '.'], check=True)
 
-shutil.move("mouseLiver.zarr", "data.zarr")
+# reading and rewriting ensures we are writing according to the latest format
+src_path = Path('mouseLiver.zarr')
+des_path = Path('data.zarr')
 
-print("Extraction complete.")
+sdata = sd.read_zarr(src_path)
+print(sdata)
+
+if src_path.exists():
+    shutil.rmtree(des_path)
+
+sdata.write(des_path)
+
+# cleanup
+if src_path.exists():
+    shutil.rmtree(src_path)
+
+print("Write to Zarr complete.")
