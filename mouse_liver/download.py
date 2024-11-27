d353d1db8c1fee95923d28fd1443b8b5aedb2ef1
Luca Marconato <m.lucalmer@gmail.com>
2024-11-27 14:28:03 +0100

    fix typo

--- mouse_liver/download.py
+++ mouse_liver/download.py
@@ -14,4 +14,4 @@ data_dir.mkdir(parents=True, exist_ok=True)
 zip_path = data_dir / "mouseLiver.zarr.zip"
 subprocess.run(["curl", "-o", zip_path, url], check=True)
 
-rint("Download complete.")
+print("Download complete.")
