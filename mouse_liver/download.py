72538c363b08ef2ad97611489438840e6bb8dc46
Luca Marconato <m.lucalmer@gmail.com>
2024-11-27 11:17:56 +0100

    add mouse liver dataset from Lotte

---
+++ mouse_liver/download.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+from pathlib import Path
+import shutil
+
+# URL of the dataset
+url = "https://s3.embl.de/spatialdata/raw_data/mouseLiver.zarr.zip"
+
+# Create the data directory if it doesn't exist
+data_dir = Path("data")
+data_dir.mkdir(parents=True, exist_ok=True)
+
+# Download the dataset
+zip_path = data_dir / "mouseLiver.zarr.zip"
+# subprocess.run(["curl", "-o", zip_path, url], check=True)
+
+# Unzip the dataset into the extraction directory
+subprocess.run(["unzip", "-o", zip_path, "-d", '.'], check=True)
+
+shutil.move("mouseLiver.zarr", "data.zarr")
+
+print("Download and extraction complete.")
