ce4bcf6fc4430ab25c551895c25bf3cff1523acc
Luca Marconato <m.lucalmer@gmail.com>
2025-01-15 18:14:41 +0100

    fix spacem datasets

--- spacem_scseahorse1/download.py
+++ spacem_scseahorse1/download.py
@@ -7,5 +7,7 @@ URL = "https://s3.embl.de/spatialdata/raw_data/20220121_ScSeahorse1.small.zip"
 os.chdir(os.path.dirname(__file__))
 command = f"curl {URL} --output 'data.zip'"
 subprocess.run(command, shell=True, check=True)
-subprocess.run("unzip -o data.zip", shell=True, check=True)
-subprocess.run("mv spatialdata.zarr data.zarr", shell=True, check=True)
+os.makedirs('data', exist_ok=True)
+os.rename('data.zip', 'data/data.zip')
+subprocess.run("unzip -o data/data.zip -d data/", shell=True, check=True)
+subprocess.run("mv data/spatialdata.zarr data/data.zarr", shell=True, check=True)
--- spacem_scseahorse1/to_zarr.py
+++ spacem_scseahorse1/to_zarr.py
@@ -3,12 +3,15 @@
 from pathlib import Path
 import spatialdata as sd
 
-# Dataset is already in SpatialData format.
-path_read = Path(__file__).parent / "data.zarr"
-assert path_read.exists()
+# Dataset is already in SpatialData format. Still, read it and convert it to the latest version
+path_read = Path(__file__).parent / "data/data.zarr"
+path_write = Path(__file__).parent / "data.zarr"
 
-print(f'view with "python -m napari_spatialdata view data.zarr"')
+assert path_read.exists()
+sdata = sd.SpatialData.read(path_read)
 
 # Test reading
-sdata = sd.SpatialData.read("./data.zarr")
 print(sdata)
+
+sdata.write(path_write, overwrite=True)
+print(f'view with "python -m napari_spatialdata view data.zarr"')
